599df98797cce062bf01c37c7ae772c87efaf042
cheewell <che@ewell.finance>
2024-01-27 18:57:32 +0800

    try1

--- src/EwellServer.EntityEventHandler.Core/Background/BackgroundJobs/CancelProjectJob.cs
+++ src/EwellServer.EntityEventHandler.Core/Background/BackgroundJobs/CancelProjectJob.cs
@@ -1,4 +1,7 @@
+using System;
 using System.Threading.Tasks;
+using AElf.Indexing.Elasticsearch;
+using EwellServer.Entities;
 using EwellServer.EntityEventHandler.Core.Background.BackgroundJobs.BackgroundJobDescriptions;
 using EwellServer.EntityEventHandler.Core.Background.Services;
 using Microsoft.Extensions.Logging;
@@ -11,16 +14,31 @@ public class CancelProjectJob : IAsyncBackgroundJob<CancelProjectJobDescription>
 {
     private readonly IScriptService _scriptService;
     private readonly ILogger<CancelProjectJob> _logger;
+    private readonly INESTRepository<CrowdfundingProjectIndex, string> _crowdfundingProjectIndexRepository;
 
-    public CancelProjectJob(IScriptService scriptService, ILogger<CancelProjectJob> logger)
+    public CancelProjectJob(IScriptService scriptService, ILogger<CancelProjectJob> logger, INESTRepository<CrowdfundingProjectIndex, string> crowdfundingProjectIndexRepository)
     {
         _scriptService = scriptService;
         _logger = logger;
+        _crowdfundingProjectIndexRepository = crowdfundingProjectIndexRepository;
     }
 
     public async Task ExecuteAsync(CancelProjectJobDescription args)
     {
-        _logger.LogInformation("CancelProjectJobDescription args={args}", args);
+        _logger.LogInformation("ExecuteAsyncCancelProjectJobDescription args={args}", args);
+        try
+        {
+            _logger.LogInformation("ExecuteAsyncCancelProjectJobDescriptionAddEs args={args}", args);
+            await _crowdfundingProjectIndexRepository.AddOrUpdateAsync(new CrowdfundingProjectIndex
+            {
+                Id = args.Id,
+                PeriodDuration = 3
+            });
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e,"ExecuteAsyncCancelProjectJobDescription id={id}",args.Id);
+        }
         await _scriptService.ProcessCancelProjectAsync(args);
     }
 }
