15dcb4f7245d46e45b5a43ccf43c3d0ad214a860
wolf <wolf wolf@ewell.finance>
2024-02-20 19:20:27 +0800

    add return ActualClaimAmount

--- src/EwellServer.Application.Contracts/Project/Dto/QueryProjectResultDto.cs
+++ src/EwellServer.Application.Contracts/Project/Dto/QueryProjectResultDto.cs
@@ -180,6 +180,7 @@ public class QueryProjectResultBaseDto
             InvestCreateTime = userProject.CreateTime;
             LiquidatedDamageAmount = userProject.LiquidatedDamageAmount;
             ClaimedLiquidatedDamage = userProject.ClaimedLiquidatedDamage;
+            ActualClaimAmount = userProject.ActualClaimAmount;
         }
     }
 }
