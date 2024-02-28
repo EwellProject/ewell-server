a5f3e50c7258e6f351efaa6b8e1e504258ce40c2
wolf <wolf wolf@ewell.finance>
2024-02-28 10:40:20 +0800

    query user project add resp params

--- src/EwellServer.Application.Contracts/Project/Dto/QueryProjectUserResultDto.cs
+++ src/EwellServer.Application.Contracts/Project/Dto/QueryProjectUserResultDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using EwellServer.Token;
 
 namespace EwellServer.Project.Dto;
 
@@ -9,13 +10,26 @@ public class QueryProjectUserResultDto
     public long TotalAmount { get; set; } = 0;
     public long TotalUser { get; set; } = 0;
     public List<ProjectUserDto> Users { get; set; } = new();
+
     public string VirtualAddress { get; set; } = "";
+
+    public long CurrentRaisedAmount { get; set; }
+
+    public long CurrentCrowdFundingIssueAmount { get; set; }
+
+    public long TargetRaisedAmount { get; set; }
+
+    public long CrowdFundingIssueAmount { get; set; }
+
+    public TokenBasicInfo ToRaiseToken { get; set; }
+
+    public TokenBasicInfo CrowdFundingIssueToken { get; set; }
 }
 
 public class ProjectUserDto
 {
-    public string Address { get; set; } 
-    public long InvestAmount { get; set; } 
+    public string Address { get; set; }
+    public long InvestAmount { get; set; }
     public DateTime CreateTime { get; set; }
     public string Symbol { get; set; } = "ELF";
     public int Decimals { get; set; } = 8;
